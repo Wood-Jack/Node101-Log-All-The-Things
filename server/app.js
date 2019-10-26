cd1a2f772107de81e258e2c1aed396ff0b8afee1
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-10-26 15:54:40 -0700

    middleware successfully grabs data and sends it to csv file

--- server/app.js
+++ server/app.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
+
 const app = express();
 
 
 app.use(morgan('dev'));
-// app.use((req, res, next) => {
-// // write your logging code here
 
-// });
+app.use((req, res, next) => {
+    fs.appendFile('log.csv',
+     `\n${req.headers['user-agent']},` +
+     new Date() + ',' +
+     req.method + ',' +
+     req.path + ',' +
+     req.protocol + ',' +
+     res.statusCode + ',',
+     (error) => {
+        console.log(error)
+    })
+    next();
+});
+
 
 app.get('/', (req, res) => {
-res.status(200).send('ok')
+ res.status(200).send('ok')
 });
 
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
-
 });
 
 module.exports = app;
