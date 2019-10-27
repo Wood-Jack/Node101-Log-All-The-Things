d2065dc522d6ea43088864856fcbaed3cea267d3
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-10-27 13:57:07 -0700

    /logs route successfully displays json data

--- server/app.js
+++ server/app.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
+const csv = require('csvtojson');
 
 const app = express();
-
 
 app.use(morgan('dev'));
 
 app.use((req, res, next) => {
     fs.appendFile('log.csv',
-     `\n${req.headers['user-agent']},` +
-     new Date() + ',' +
+     `\n${req.headers['user-agent'].replace(',','')},` +
+     new Date().toISOString() + ',' +
      req.method + ',' +
      req.path + ',' +
-     req.protocol + ',' +
-     res.statusCode + ',',
+     "HTTP/" + req.httpVersion + ',' +
+     res.statusCode,
      (error) => {
-        console.log(error)
+        console.log('error:', error)
     })
     next();
 });
-
 
 app.get('/', (req, res) => {
  res.status(200).send('ok')
 });
 
 app.get('/logs', (req, res) => {
+    csv().fromFile('log.csv').then(json => res.status(200).send(json))
 });
 
 module.exports = app;
